ca084c6600b2fcde4caff217cbd373dae134856e
Vladimir Avdoshka <vavdoshka@kyriba.com>
2020-04-09 22:30:26 +0200

    refactored form, yet withou css

--- App.js
+++ App.js
@@ -1,9 +1,52 @@
-import * as React from 'react';
+import * as React from 'react'
+
 import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
+
+
+
+const Row = ({children}) => {
+  return (<View>{children}</View>)
+}
+
+const useInput = initialState => {
+    const [value, setValue] = React.useState(initialState);
+    const onChange = e => setValue(e.target.value)
+    return[value, onChange, setValue]
+}
+
+
+  const useInputField = type => {
+    const initialState = '';
+    
+    const [value, onInputChange, setValue] = useInput(initialState);
+
+    const input = (name, placeholder) => {
+        return (
+          <Row>
+            <Text style={styles.inputLabel}>{name}</Text>
+            <TextInput style={styles.input}
+                name={name}
+                value={value}
+                onChange={onInputChange}
+                type={type}
+                placeholder={placeholder}
+            />
+            </Row>
+        );
+    };
+
+    const reset = () => {
+        setValue(initialState);
+    };
+
+    return [value, input, reset, setValue];
+
+
+}
 
 export default function App() {
   const  handleClick = async () => {
@@ -28,25 +71,28 @@ export default function App() {
       console.log("Generate")
     }
 
+    const [firstName, inputfirstName] = useInputField('text')
+    const [lastName, inputLastName] = useInputField('text')
+    const [bithDate, inputBithDate] = useInputField('date')
+    const [birthPlace, inputBirthPlace] = useInputField('text')
+    const [address, inputAddress] = useInputField('text')
+    const [city, inputCity] = useInputField('text')
+    const [postCode, inputpostCode] = useInputField('text')
+
     return (
     <View style={styles.container}>
+      <Row>
       <Text style={styles.title1}>Remplissez en ligne votre attestation numérique :</Text>
       <Text style={styles.title2}>Tous les champs sont obligatoires.</Text>
-      <Text style={styles.inputLabel}>Prénom</Text>
-      <TextInput style={styles.input} placeholder="Jean"/>
-      <Text style={styles.inputLabel}>Nom</Text>
-      <TextInput style={styles.input} placeholder="Dupont"/>
-      <Text style={styles.inputLabel}>Date de naissance (au format jj/mm/aaaa)</Text>
-      <TextInput style={styles.input} placeholder="01/01/1970"/>
-      <Text style={styles.inputLabel}>Lieu de naissance</Text>
-      <TextInput style={styles.input} placeholder="Lyon"/>
-      <Text style={styles.inputLabel}>Adresse</Text>
-      <TextInput style={styles.input} placeholder="999 avenue de france"/>
-      <Text style={styles.inputLabel}>Ville</Text>
-      <TextInput style={styles.input} placeholder="Paris"/>
-      <Text style={styles.inputLabel}>Code Postal</Text>
-      <TextInput style={styles.input} placeholder="75001"/>
-      <Button onClick={handleGenerate()} >Generate qrcode</Button>
+      </Row>
+      {inputfirstName('Prénom', 'Jean')}
+      {inputLastName('Nom', 'Dupont')}
+      {inputBithDate('Date de naissance (au format jj/mm/aaaa)', '01/01/1970')}
+      {inputBirthPlace('Lieu de naissance', 'Lyon')}
+      {inputAddress('Adresse', '999 avenue de france')}
+      {inputCity('Ville', 'Paris')}
+      {inputpostCode('Code Postal', '75001')}
+      <Button onClick={handleGenerate()} title="Press Me" >Generate qrcode</Button>
     </View>
   );
 }
@@ -65,7 +111,7 @@ const styles = StyleSheet.create({
     color: "red",
   },
   row: {
-    display: "flex"
+    display: "flex",
   }
 
 });
