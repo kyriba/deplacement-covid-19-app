2a0380afee69f91e88a74911869e48fe4b3888f7
igor.lizura <ilizura@kyriba.com>
2020-04-17 17:07:28 +0200

    Chinese translation was added

--- App.js
+++ App.js
@@ -11,6 +11,28 @@ import PersonalForm from "./src/components/PersonalForm";
 const Stack = createStackNavigator();
 
 i18n.translations = {
+  zh: {
+  firstName: "名",
+  lastName: "姓",
+  attestation: "请填写您的数字证书(digital certificate)",
+  filds: "所有字段为必填",
+  birthday: "出生年月日 (按照 dd / mm / yyyy 格式)",
+  place: "出生地",
+  address: "地址",
+  city: "城市",
+  postCode: "邮编",
+  submit: "提交",
+  reason: "请选择出行原因",
+  travail: "在家和工作地之间的通勤，或无法推迟的商务出差，并且所从事的工作无法以远程工作的方式进行。" ,
+  courses: "外出至得到授权的场所，购买与工作活动或生活相关的必需品。",
+  sante: "无法远程进行或无法推迟的医疗护理和问诊；对需要长期进行护理或问诊的病人所进行的医疗活动。",
+  famille: "出于不可抗拒的家庭原因，出行援助弱势人群或照管儿童。",
+  sport: "每天不超过一小时，离家不超过一公里范围内的活动。活动仅限于不与他人近距离接触或非群体聚集的个人体育运动；亦或是与唯一的同居者散步，或遛狗等满足宠物需求的活动",
+  judiciaire: "司法或行政传唤.",
+  missions: "应行政当局要求参与与公众利益相关的任务.",
+  certificate: "证书",
+  form: "表格"
+  },
   en: {
     firstName: "First name",
     lastName: "Last name",
